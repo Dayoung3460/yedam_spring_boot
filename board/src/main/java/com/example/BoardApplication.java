19c565ab7b350ed99f3d77c4d9d741b7fa6af8e9
Dayoung <kimdayoung771@gmail.com>
2025-01-17 14:44:25 +0900

    add aop dependency

--- board/src/main/java/com/example/BoardApplication.java
+++ board/src/main/java/com/example/BoardApplication.java
@@ -3,9 +3,12 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.example.**.mapper")
+// AOP 설정
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class BoardApplication {
 
 	public static void main(String[] args) {
